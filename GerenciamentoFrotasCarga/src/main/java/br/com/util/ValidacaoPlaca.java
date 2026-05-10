62d78776de9725a7e4b83911c5a8dfeaa2f3b497
GilvanPedro <gilvanpedro2006@gmail.com>
2026-05-10 14:42:05 -0300

    verificacoes de placa

--- GerenciamentoFrotasCarga/src/main/java/br/com/util/ValidacaoPlaca.java
+++ GerenciamentoFrotasCarga/src/main/java/br/com/util/ValidacaoPlaca.java
@@ -1,5 +1,53 @@
 package br.com.util;
 
 public class ValidacaoPlaca {
+    /*
+        MODELO ANTIGO:
+        ABC-1234
 
+        MODELO MERCOSUL:
+        ABC1D23
+
+        A validação aceita:
+        ABC-1234
+        ABC1234
+        ABC1D23
+        ABC-1D23
+    */
+
+    // Carro
+    public static boolean validarPlacaCarro(String placa) {
+
+        placa = placa.toUpperCase().replace("-", "");
+
+        return placa.matches("^[A-Z]{3}[0-9]{4}$") || // antigo
+                placa.matches("^[A-Z]{3}[0-9][A-Z][0-9]{2}$"); // mercosul
+    }
+
+    // Moto
+    public static boolean validarPlacaMoto(String placa) {
+
+        placa = placa.toUpperCase().replace("-", "");
+
+        return placa.matches("^[A-Z]{3}[0-9]{4}$") ||
+                placa.matches("^[A-Z]{3}[0-9][A-Z][0-9]{2}$");
+    }
+
+    // Van
+    public static boolean validarPlacaVan(String placa) {
+
+        placa = placa.toUpperCase().replace("-", "");
+
+        return placa.matches("^[A-Z]{3}[0-9]{4}$") ||
+                placa.matches("^[A-Z]{3}[0-9][A-Z][0-9]{2}$");
+    }
+
+    // Caminhão
+    public static boolean validarPlacaCaminhao(String placa) {
+
+        placa = placa.toUpperCase().replace("-", "");
+
+        return placa.matches("^[A-Z]{3}[0-9]{4}$") ||
+                placa.matches("^[A-Z]{3}[0-9][A-Z][0-9]{2}$");
+    }
 }
