7c84783e9910dde123cc08fcff0209460f709880
GilvanPedro <gilvanpedro2006@gmail.com>
2026-05-10 14:36:09 -0300

    validacao de email e de cpf

---
+++ GerenciamentoFrotasCarga/src/main/java/br/com/util/ValidacaoEmail.java
@@ -0,0 +1,16 @@
+package br.com.util;
+
+public class ValidacaoEmail {
+    public static boolean validarEmail(String email){
+        if(email == null) return false;
+
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z]{2,}$";
+
+        if(!email.matches(regex)) return false;
+
+        if(email.contains("..")) return false;
+        if(email.startsWith(".") || email.endsWith(".")) return false;
+
+        return true;
+    }
+}
---
+++ GerenciamentoFrotasCarga/src/main/java/br/com/util/VerificacaoCpf.java
@@ -0,0 +1,36 @@
+package br.com.util;
+
+public class VerificacaoCpf {
+    public boolean verificarCpf(String cpf){
+        if(cpf == null) return false;
+
+        cpf = cpf.replaceAll("\\D", "");
+
+        if(cpf.length() != 11) return false;
+
+        String finalCpf = cpf;
+        if(cpf.chars().allMatch(c -> c == finalCpf.charAt(0))) return false;
+
+        try{
+            int soma = 0;
+            for(int i = 0; i<9; i++){
+                soma+= (cpf.charAt(i) - '0') * (10-i);
+            }
+
+            int dig1 = 11 - (soma%11);
+            if(dig1 >=10) dig1 = 0;
+
+            soma = 0;
+            for(int i = 0; i<10; i++){
+                soma += (cpf.charAt(i) - '0') * (11-i);
+            }
+
+            int dig2 = 11 - (soma%11);
+            if(dig2 >= 10) dig2 = 0;
+
+            return dig1 == (cpf.charAt(9) - '0') && dig2 == (cpf.charAt(10) - '0');
+        } catch(Exception e){
+            return false;
+        }
+    }
+}
