6ab8661092575c025ad8a3587c5db920278cfb1d
Gilvan Pedro <gilvanpedro2006@gmail.com>
2026-05-11 19:17:35 -0300

    testes no main

--- GerenciamentoFrotasCarga/src/main/java/br/com/Main.java
+++ GerenciamentoFrotasCarga/src/main/java/br/com/Main.java
@@ -1,8 +1,10 @@
 package br.com;
 
+import br.com.controller.VeiculoController;
 import br.com.model.entity.*;
 import br.com.model.enums.TamanhoCarro;
 import br.com.model.enums.TamanhoMoto;
+import br.com.model.enums.TipoVeiculo;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,41 +29,20 @@ public class Main {
         System.out.println("\nApós alterações:");
         System.out.println(entregador);
 
-        Moto moto = new Moto(
-                2,
+        VeiculoController veiculoController = new VeiculoController();
+
+        veiculoController.adicionarVeiculo(
                 "Honda",
                 "CG 160",
                 "XYZ9K88",
                 false,
                 15.0,
                 120.0,
-                35.0
+                35.0,
+                TipoVeiculo.MOTO
         );
 
-        Caminhao caminhao = new Caminhao(
-                2,
-                "Honda",
-                "CG 160",
-                "RBV8I82",
-                false,
-                15.0,
-                15600,
-                20.0
-        );
 
-        Veiculo veiculo = new Carro(
-                1,
-                "Toyota",
-                "Corolla",
-                "ABC1D23",
-                true,
-                50.0,
-                1340.0,
-                12.5
-        );
-
-        System.out.println(veiculo);
-        System.out.println(moto);
-        System.out.println(caminhao);
+        veiculoController.mostrarVeiculo();
     }
 }
--- GerenciamentoFrotasCarga/src/main/java/br/com/controller/VeiculoController.java
+++ GerenciamentoFrotasCarga/src/main/java/br/com/controller/VeiculoController.java
@@ -6,6 +6,7 @@ import br.com.util.ValidacaoPlaca;
 
 public class VeiculoController {
     ValidacaoPlaca validacaoPlaca = new ValidacaoPlaca();
+    Veiculo novoVeiculo;
 
     // Adicionar Veiculo
     public String adicionarVeiculo(String marca, String modelo, String placa, boolean automatico, double combustivelMaximo, double peso, double kmPorLitro, TipoVeiculo tipoVeiculo){
@@ -29,8 +30,6 @@ public class VeiculoController {
         if(kmPorLitro<=0){
             return "Adicione uma quantidade de quilometros por litro válida";
         }
-
-        Veiculo novoVeiculo;
 
         switch (tipoVeiculo){
             case MOTO:
@@ -84,6 +83,9 @@ public class VeiculoController {
         return "Veículo cadastrado com sucesso";
     }
 
+    public void mostrarVeiculo(){
+        System.out.println(novoVeiculo);
+    }
 
     // Vai verificar algumas informações bases
     public boolean verificacaoNula(String marca, String modelo, String placa, double combustivelMaximo, double kmPorLitro){
